40c25cfa140c3fe81d67f9e9b9f0aaccd27295f0
Gabriel <gabriel.polidoro123@outlook.com>
2025-02-03 19:11:32 -0300

    change links and new deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import '../Navbar.css';
 function Navbar() {
   return (
     <div className="navbar">
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/gabee__muller/" target="_blank" rel="noopener noreferrer">
         <FaInstagram />
       </a>
-      <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/gabriel-muller-polidoro-327033208/" target="_blank" rel="noopener noreferrer">
         <FaLinkedin />
       </a>
-      <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/gabrielmuller01" target="_blank" rel="noopener noreferrer">
         <FaGithub />
       </a>
     </div>
